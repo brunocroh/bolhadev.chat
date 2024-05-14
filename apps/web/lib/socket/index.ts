eac8088dcad3bb315dfd481bab33b39d46e9ae01
Bruno Rodrigues <brunocroh@gmail.com>
2024-05-14 01:18:44 +0100

    chore: add env to WEBSOCKET_URL

--- apps/web/lib/socket/index.ts
+++ apps/web/lib/socket/index.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const URL = "http://localhost:4000";
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 
 export const socket = (): Socket => {
   return io(URL);
