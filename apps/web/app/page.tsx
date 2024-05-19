469bdc51b15c1eadf90291b5dfbd4b4cda61ed1c
Bruno Rodrigues <brunocroh@gmail.com>
2024-05-19 18:21:03 +0100

    feat(web): adjust light ray

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -6,25 +7,30 @@ export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col h-full">
       <div>
-        <img
+        <Image
           src="/light-ray.svg"
+          alt="Purple light ray"
+          width="1024"
+          height="800"
           className="absolute animate-in fade-in slide-in-from-bottom-1"
-          style={{ color: "transparent", animationDuration: "5s" }}
+          style={{ color: "transparent", animationDuration: "10s" }}
         />
         <div className="flex flex-row w-full">
-          <video
-            className=""
-            style={{
-              display: "block",
-              width: "920px",
-              height: "700px",
-            }}
-            playsInline
-            loop={true}
-            autoPlay={true}
-            muted={true}
-            src="/earth.mp4"
-          ></video>
+          <div className="flex">
+            <video
+              className="w-full max-w-2xl rounded-lg animation-scale-in-fade"
+              style={{
+                display: "block",
+                width: "920px",
+                height: "700px",
+              }}
+              playsInline
+              loop={true}
+              autoPlay={true}
+              muted={true}
+              src="/earth.mp4"
+            ></video>
+          </div>
           <div className="content-center">
             <Link href={`room/queue`}>
               <Button className="bg-red-500 min-w-max">Start a chat</Button>
