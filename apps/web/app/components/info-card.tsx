34ba5d91ed5dca344ce6cc5d19a5feee2212f81c
Bruno Rodrigues <brunocroh@gmail.com>
2024-05-26 10:58:00 +0100

    feat(web): improve InfoCard

--- apps/web/app/components/info-card.tsx
+++ apps/web/app/components/info-card.tsx
@@ -11,7 +11,7 @@ type InfoCard = {
 export const InfoCard: React.FC<InfoCard> = ({icon, content}) => {
   const Icon = icon
   return (
-    <Card className="w-full md:w-2/5">
+    <Card className="border-slate-5 bg-slate-6 w-full border border-b-0 md:w-2/5 " style={{background: "linear-gradient(180deg, rgba(80, 80, 80, 0.15) 0%, rgba(0, 0, 0, 0) 70%"}}>
       <CardContent className="flex flex-row items-center gap-4 p-5">
         <div className="border-slate-6 flex items-center justify-center rounded-md border bg-gradient-to-b from-white/[3%] to-violet-500/10 p-4 transition duration-200 ease-in-out">
           <Icon size={26} />
