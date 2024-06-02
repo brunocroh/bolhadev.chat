463cc6b163f59c0409de601bfdf621d44b76418f
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-02 12:26:40 +0100

    feat(web): add new logo to header component

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { Github } from 'lucide-react'
 
 export const Header = () => {
   return (
-    <nav className="container z-10 flex justify-between p-5">
+    <nav className="container z-10 flex items-center justify-between p-5">
       <Link href="/">
-        <h1 className="text-base font-semibold text-white">Bolhadev.chat</h1>
+        <Image src="/full-logo.png" width={101} height={42}></Image>
       </Link>
       <Link href="https://github.com/brunocroh/bolhadev.chat">
         <h2 className="rounded-lg p-1 hover:bg-white hover:text-black">
