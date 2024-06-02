2f0a6888ac9e8978f62aa51801202c470dc45d40
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-02 12:29:46 +0100

    feat(web): add alt to logo

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -7,7 +7,12 @@ export const Header = () => {
   return (
     <nav className="container z-10 flex items-center justify-between p-5">
       <Link href="/">
-        <Image src="/full-logo.png" width={101} height={42}></Image>
+        <Image
+          src="/full-logo.png"
+          width={101}
+          height={42}
+          alt="BolhaDEV.chat logo featuring a stylized globe and a speech bubble with a language symbol on a dark background."
+        ></Image>
       </Link>
       <Link href="https://github.com/brunocroh/bolhadev.chat">
         <h2 className="rounded-lg p-1 hover:bg-white hover:text-black">
