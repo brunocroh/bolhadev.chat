ae163f1bb285ae7e9d7480ddb594627b7de169f1
matheusTA <matrajano12@gmail.com>
2024-06-02 17:14:46 -0300

    refactor(env-config): removing server env from client env

--- packages/env-config/index.ts
+++ packages/env-config/index.ts
@@ -2,15 +2,17 @@ import { z } from 'zod'
 import { createEnv } from '@t3-oss/env-nextjs'
 
 export const env = createEnv({
+  server: {
+    NEXT_SUPABASE_URL: z.string(),
+    NEXT_SUPABASE_KEY: z.string(),
+  },
   client: {
     NEXT_PUBLIC_SOCKET_URL: z.string().default('ws://localhost:4000'),
-    NEXT_PUBLIC_SUPABASE_URL: z.string(),
-    NEXT_PUBLIC_SUPABASE_KEY: z.string(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
+    NEXT_SUPABASE_URL: process.env.NEXT_SUPABASE_URL,
+    NEXT_SUPABASE_KEY: process.env.NEXT_SUPABASE_KEY,
   },
   emptyStringAsUndefined: true,
 })
