8e77d3d401361bb28687c1644f55d5a99f51c7e2
matheusTA <matrajano12@gmail.com>
2024-06-02 16:08:48 -0300

    refactor(env-config): change socket default port and supabase key variable name

--- packages/env-config/index.ts
+++ packages/env-config/index.ts
@@ -1,17 +1,16 @@
-import { createEnv } from "@t3-oss/env-nextjs"
-import { z } from "zod"
+import { z } from 'zod'
+import { createEnv } from '@t3-oss/env-nextjs'
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_SOCKET_URL: z.string().default("ws://localhost:4001"),
+    NEXT_PUBLIC_SOCKET_URL: z.string().default('ws://localhost:4000'),
     NEXT_PUBLIC_SUPABASE_URL: z.string(),
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+    NEXT_PUBLIC_SUPABASE_KEY: z.string(),
   },
-  shared: {},
   runtimeEnv: {
     NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
   },
   emptyStringAsUndefined: true,
 })
