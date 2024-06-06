efd7711815325800fd09e1db7a3c53f5b0f36c58
Fernando Kawano <kawano.fer@gmail.com>
2024-06-06 19:10:41 -0300

    change github icon position

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -14,7 +14,11 @@ export const Header = () => {
           alt="BolhaDEV.chat logo featuring a stylized globe and a speech bubble with a language symbol on a dark background."
         ></Image>
       </Link>
-      <Link href="https://github.com/brunocroh/bolhadev.chat">
+      <Link
+        href="https://github.com/brunocroh/bolhadev.chat"
+        target="_blank"
+        className="absolute right-0 top-0"
+      >
         <GithubCorner />
       </Link>
     </nav>
