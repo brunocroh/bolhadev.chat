3404bd561bd2fa52ea5e6501ede300094707fdf8
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-09 20:28:08 +0100

    feat(@repo/env-config): add NEXT_PUBLIC_SUPABASE_ANON_KEY to envs

--- packages/env-config/index.ts
+++ packages/env-config/index.ts
@@ -8,9 +8,11 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_SOCKET_URL: z.string().default('ws://localhost:4000'),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_SUPABASE_URL: process.env.NEXT_SUPABASE_URL,
     NEXT_SUPABASE_KEY: process.env.NEXT_SUPABASE_KEY,
   },
