8f74e8e83f8b5abb053804ea6b2c43b209b0202f
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-09 20:15:37 +0100

    chore(web): remove unused console.log

--- apps/web/app/auth/callback/route.ts
+++ apps/web/app/auth/callback/route.ts
@@ -5,7 +5,6 @@ import { type CookieOptions, createServerClient } from '@supabase/ssr'
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
 
-  console.log({})
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/'
