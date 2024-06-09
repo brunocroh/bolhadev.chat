d12e7ba57d975fc476474b6f1b8d24a32c913b6f
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-09 16:17:17 +0100

    feat(web): adjust text style on login  screen

--- apps/web/app/auth/page.tsx
+++ apps/web/app/auth/page.tsx
@@ -6,8 +6,8 @@ export default function Page(): JSX.Element {
   return (
     <div className="mt-16 flex w-full max-w-2xl flex-col items-center gap-6 self-center">
       <div className="text-center">
-        <h2>Login</h2>
-        <p>Sign in your account</p>
+        <h2 className="text-2xl font-bold">Login</h2>
+        <p className="text-sm text-slate-100">Sign in your account</p>
       </div>
       <div className="w-full max-w-sm">
         <Button className="flex h-12 w-full gap-2 rounded-full align-top text-base font-normal text-black">
