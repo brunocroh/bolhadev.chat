a5a4c3eecde57f0643990478ebe996c2d139635e
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-10 22:29:30 +0100

    chore(web): rename favicon and change middle rules

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -12,8 +12,8 @@ export const config = {
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
+     * - icon.ico (favicon file)
      */
-    '/((?!_next/static|_next/image|favicon.ico|icon.ico).*)',
+    '/((?!_next/static|_next/image|icon.ico).*)',
   ],
 }
