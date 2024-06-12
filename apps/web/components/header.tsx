e2bdc3aeb9a1a57a82f21e6fc1b348921edd5594
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-12 23:56:51 +0100

    feat(web): add right padding to header to prevent github corner be in front of content

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -2,23 +2,22 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { ArrowRight } from 'lucide-react'
+import ProfileButton from '@/app/auth/components/profile-button'
 import SignOut from '@/app/auth/components/sign-out'
 import { Button } from '@/components/ui/button'
 import { User } from '@supabase/supabase-js'
-import ProfileButton from '@/app/auth/components/profile-button'
 
 type Header = {
   user: User
 }
 
 export const Header: React.FC<Header> = ({ user }) => {
-
-  console.log({user})
+  console.log({ user })
 
   const authenticated = Boolean(user)
 
   return (
-    <nav className="w-full border-b border-b-zinc-800 py-6">
+    <nav className="w-full border-b border-b-zinc-800 py-6 pr-6">
       <div className="container flex items-start justify-between">
         <Link href="/">
           <Image
@@ -28,21 +27,24 @@ export const Header: React.FC<Header> = ({ user }) => {
             alt="BolhaDEV.chat logo featuring a stylized globe and a speech bubble with a language symbol on a dark background."
           ></Image>
         </Link>
-        <div className="self-center h-full">
-
-        {authenticated ? (
-          <ProfileButton name={user.user_metadata.name} userName={user.user_metadata.preferred_username} photo={user.user_metadata.avatar_url} />
-        ): (
-            <Link href="room/queue" className="invisible self-center md:visible">
+        <div className="h-full self-center">
+          {authenticated ? (
+            <ProfileButton
+              name={user.user_metadata.name}
+              userName={user.user_metadata.preferred_username}
+              photo={user.user_metadata.avatar_url}
+            />
+          ) : (
+            <Link
+              href="room/queue"
+              className="invisible self-center md:visible"
+            >
               <Button className="rounded-xl p-6 text-zinc-800">
                 <span className="mr-1 text-base leading-6">Get started</span>
                 <ArrowRight />
               </Button>
             </Link>
-          ) 
-
-        }
-
+          )}
         </div>
       </div>
     </nav>
