0e8955a045bbe2c143b8b8c300b7cb13d1a67549
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-12 23:30:39 +0100

    feat: validate if user authenticated on chat video room

--- apps/web/app/room/[slug]/page.tsx
+++ apps/web/app/room/[slug]/page.tsx
@@ -1,6 +1,15 @@
+import { redirect } from 'next/navigation'
+import { createClient } from '@/lib/supabase/server'
 import { VideoChat } from './components/video-chat'
 
-export default function Page(): JSX.Element {
+export default async function Page(): JSX.Element {
+  const supabase = createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+
+  if (error || !data?.user) {
+    redirect('/auth')
+  }
   return (
     <section className="container flex h-full flex-col content-center items-center justify-center gap-4">
       <VideoChat />
