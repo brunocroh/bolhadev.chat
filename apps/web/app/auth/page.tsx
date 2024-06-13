769b3701644066e1dbf580c6d54878814dda0313
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-13 22:25:09 +0100

    feat(web): add sign in email/password form to auth screen

--- apps/web/app/auth/page.tsx
+++ apps/web/app/auth/page.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import GithubSignIn from './components/github-sign-in'
-import SignOut from './components/sign-out'
+import { Separator } from '@/components/ui/separator'
+import EmailSignIn from './components/email-sign-in'
 
 export default function Page(): JSX.Element {
   return (
-    <div className="mt-16 flex w-full max-w-2xl flex-col items-center gap-6 self-center">
-      <div className="text-center">
+    <div className="mt-16 flex w-full max-w-sm flex-col px-6 items-center gap-6 self-center">
+      <div className="text-center flex flex-col gap-2">
         <h2 className="text-2xl font-bold">Login</h2>
         <p className="text-sm text-slate-100">Sign in your account</p>
       </div>
-      <div className="w-full max-w-sm">
+      <div className="w-full">
         <GithubSignIn />
-        <SignOut />
+      </div>
+      <div className="flex w-full items-center gap-3">
+        <div className="w-full">
+          <Separator/>
+        </div>
+        <span>OR</span>
+        <div className="w-full text-xs text-slate-200">
+          <Separator/>
+        </div>
+      </div>
+      <div className="w-full">
+        <EmailSignIn />
       </div>
     </div>
   )
