c2eba3ed51be26f71032e1c55959aec22f962480
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-14 23:48:38 +0100

    chore(web): remove unused imports on header

--- apps/web/components/header/index.tsx
+++ apps/web/components/header/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { ArrowRight } from 'lucide-react'
 import ProfileButton from '@/app/auth/components/profile-button'
-import { Button } from '@/components/ui/button'
 import { User } from '@supabase/supabase-js'
 import { HeaderCTA } from './HeaderCTA'
 
