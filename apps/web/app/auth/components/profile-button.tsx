ae1d5d85a78a9ecd4cee5cf4f4b358ae12afd56b
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-16 23:53:18 +0100

    feat(web): placeholder for profile button when user profile is not complete

--- apps/web/app/auth/components/profile-button.tsx
+++ apps/web/app/auth/components/profile-button.tsx
@@ -1,8 +1,9 @@
 'use server'
 
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
-import Image from "next/image"
+import Image from 'next/image'
+import Link from 'next/link'
+import { CircleUser } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 type ProfileButton = {
   name: string
@@ -10,18 +11,44 @@ type ProfileButton = {
   photo: string
 }
 
-export default async function ProfileButton({ name, userName, photo}: ProfileButton) {
+export default async function ProfileButton({
+  name,
+  userName,
+  photo,
+}: ProfileButton) {
   return (
     <Link href="/profile">
       <Button
         type="submit"
-        className="flex h-12 w-full gap-2 rounded-lg items-center text-base font-normal text-black"
+        className="flex h-12 w-full items-center gap-2 rounded-lg text-base font-normal text-black"
       >
-        <Image src={photo} width={32} height={32} className="rounded-full" alt="user picture"/>
-        <div className="flex flex-col justify-start">
-          <span className="text-black text-md text-bold leading-2 text-start">{name}</span>
-          <span className="text-gray-700 text-sm text-normal leading-none text-start">{userName}</span>
-        </div>
+        {photo ? (
+          <Image
+            src={photo}
+            width={32}
+            height={32}
+            className="rounded-full"
+            alt="user picture"
+          />
+        ) : (
+          <CircleUser height={32} width={32} />
+        )}
+        {name ? (
+          <div className="flex flex-col justify-start">
+            <span className="text-md text-bold leading-2 text-start text-black">
+              {name}
+            </span>
+            <span className="text-normal text-start text-sm leading-none text-gray-700">
+              {userName}
+            </span>
+          </div>
+        ) : (
+          <div className="flex flex-col justify-start">
+            <span className="text-md text-bold leading-2 text-start text-black">
+              Complete your profile
+            </span>
+          </div>
+        )}
       </Button>
     </Link>
   )
