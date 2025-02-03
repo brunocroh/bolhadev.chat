d2c9008c28112935c0d9970b54e10b8b2074dcc0
Bruno Rodrigues <brunocroh@gmail.com>
2025-02-03 23:12:29 +0000

    chore(server): update eslint config

--- apps/server/eslint.config.js
+++ apps/server/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from '@repo/eslint-config/next-js'
+import { config } from '@repo/eslint-config/base'
 
 /** @type {import("eslint").Linter.Config} */
-export default [...nextJsConfig]
+export default config
