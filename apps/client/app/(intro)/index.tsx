ff873d08341caa94b4e1f7cfe54ebcc7998ab22b
Bruno Rodrigues <brunocroh@gmail.com>
2025-02-03 23:02:22 +0000

    feat(client): add new intro routes

---
+++ apps/client/app/(intro)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Page() {
+  return (
+    <View className="p-safe bg-background">
+      <Text className="text-destructive"> text </Text>
+    </View>
+  )
+}
