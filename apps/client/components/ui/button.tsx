86949177df3846b02f0fefc6da7b2f05d22e374c
Bruno Rodrigues <brunocroh@gmail.com>
2025-02-08 21:07:33 +0000

    feat(client): animate button

--- apps/client/components/ui/button.tsx
+++ apps/client/components/ui/button.tsx
@@ -1,20 +1,55 @@
-import { Pressable, PressableProps, Text } from 'react-native'
+import { PressableProps, Text } from 'react-native'
+import { Pressable } from 'react-native'
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withSequence,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated'
 import { cn } from '@/lib/cn'
 
 type ButtonProps = {
   title: string
 } & PressableProps
 
+const DURATION = 300
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
+
 export function Button({ className, title, ...props }: ButtonProps) {
+  const transition = useSharedValue(0)
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: interpolate(transition.value, [0, 1], [1, 0.98]),
+        },
+      ],
+    }
+  })
+
   return (
-    <Pressable
+    <AnimatedPressable
       {...props}
+      style={animatedStyle}
+      onPressIn={() => {
+        transition.value = withTiming(1, { duration: DURATION })
+      }}
+      onPressOut={() => {
+        transition.value = withTiming(0, { duration: DURATION })
+      }}
       className={cn(
         'items-center justify-center rounded-md bg-primary p-2',
         className
       )}
     >
-      <Text className="text-primary-foreground">{title}</Text>
-    </Pressable>
+      <Text className="text-primary-foreground" selectable={false}>
+        {title}
+      </Text>
+    </AnimatedPressable>
   )
 }
