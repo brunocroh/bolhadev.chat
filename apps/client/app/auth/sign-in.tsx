838bd2af80b97be6f9d41988d9c29f3d1d14af97
Bruno Rodrigues <brunocroh@gmail.com>
2025-02-09 20:43:56 +0000

    feat(client): add keyboardAvoindingView to sign in screen

--- apps/client/app/auth/sign-in.tsx
+++ apps/client/app/auth/sign-in.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react'
-import { Text, View } from 'react-native'
+import { KeyboardAvoidingView, Platform, Text, View } from 'react-native'
 import { Link, useRouter } from 'expo-router'
 import { LockKeyhole, Mail } from 'lucide-react-native'
 import { Button } from '@/components/ui/button'
@@ -34,7 +34,10 @@ export default function Page() {
   }, [isLoaded, signIn, emailAddress, password, setActive, router])
 
   return (
-    <>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      style={{ flex: 1 }}
+    >
       <View className="flex-1 items-center justify-center">
         <Text className="color-primary bg-background">
           Master English, Transform Your World. The new frontier in
@@ -62,6 +65,6 @@ export default function Page() {
           <Button variant="secondary" title="Sign Up" />
         </Link>
       </View>
-    </>
+    </KeyboardAvoidingView>
   )
 }
