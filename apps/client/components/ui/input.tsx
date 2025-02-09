cf2aa50ef1b3be866e43cc253211e9ce4a164e6c
Bruno Rodrigues <brunocroh@gmail.com>
2025-02-09 20:25:53 +0000

    feat(client): increase input sizes

--- apps/client/components/ui/input.tsx
+++ apps/client/components/ui/input.tsx
@@ -12,12 +12,12 @@ function Input({ className, icon, ...props }: InputProps) {
 
   return (
     <View className="relative">
-      <View className="absolute flex h-10 w-8 items-center justify-center">
-        {Icon ? <Icon size={16} color={colors.dark.accentForeground} /> : <></>}
+      <View className="absolute flex h-full items-center justify-center pl-2">
+        {Icon ? <Icon size={24} color={colors.dark.accentForeground} /> : <></>}
       </View>
       <TextInput
         className={cn(
-          'flex h-10 flex-row gap-2 rounded-md border border-input px-3 py-1 pl-8 text-primary focus:border-accent-foreground focus:ring focus:ring-offset-2',
+          'flex h-14 flex-row gap-2 rounded-md border border-input px-3 py-1 pl-10 text-primary focus:border-accent-foreground focus:ring focus:ring-offset-2',
           className
         )}
         {...props}
