5ab5b15478e96a9d5dfac66cdcb5735825d6a2fb
Bruno Rodrigues <brunocroh@gmail.com>
2025-02-15 21:51:55 +0000

    feat(backend): init backend projet with graphql

---
+++ backend/internal/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/brunocroh/bolhadev.chat/internal/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct{
+  user model.User
+}
