7b14341fdd6b1c24b9a2123907910af2bfa82ab9
Bruno Rodrigues <brunocroh@gmail.com>
2025-02-15 23:08:47 +0000

    feat(backend): add sqlx and setup database connection

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,13 +11,44 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/brunocroh/bolhadev.chat/config"
 	"github.com/brunocroh/bolhadev.chat/internal/graph"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
 const defaultPort = "8080"
 
+var (
+	DBHOST     = os.Getenv("DB_HOST")
+	DBPORT     = 5432
+	DBNAME     = os.Getenv("DB_NAME")
+	DBUSER     = os.Getenv("DB_USER")
+	DBPASSWORD = os.Getenv("DB_PASSWORD")
+)
+
 func main() {
+  config, err := config.LoadDatabaseConfig()
+  if err != nil {
+    fmt.Errorf("Fail to load database config")
+  }
+
+
+  connectionString := config.GetConnectionString()
+  
+  fmt.Println(connectionString)
+
+  db, err := sqlx.Connect("postgres", connectionString)
+    if err != nil {
+        log.Fatalln(err)
+    }
+ if err := db.Ping(); err != nil {
+        log.Fatal(err)
+    } else {
+        log.Println("Successfully Connected")
+    }
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,8 @@ go 1.23.6
 
 require (
 	github.com/99designs/gqlgen v0.17.66
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/lib/pq v1.10.9
 	github.com/vektah/gqlparser/v2 v2.5.22
 )
 
